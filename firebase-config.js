906f54346e566d632f046736744a789433f77a82
Websites <ayaansyed8655@gmail.com>
2026-05-23 15:38:25 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBGE29YUks6sp4jZS4MzE2JIMF-RMLwVLg",
-  authDomain: "moddy-store.firebaseapp.com",
-  databaseURL: "https://moddy-store-default-rtdb.firebaseio.com",
-  projectId: "moddy-store",
-  storageBucket: "moddy-store.appspot.com",
+  apiKey: "YOUR FIREBASE API KEY HERE",
+  authDomain: "FIREBASE DOMAIN URL",
+  databaseURL: "DATABASE URL",
+  projectId: "YOUR PROJECT IT HERE",
+  storageBucket: "STORAGE URL HERE",
   messagingSenderId: "37854973622",
-  appId: "1:37854973622:web:8f927e0a1d267d099ca017",
+  appId: "FIREBASE APP ID HERE",
   measurementId: "G-FRJXCHF320"
 };
 
